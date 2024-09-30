ef4d1f48b1d99d3e0dc862d3276cc76c32690a32
Ivan Hernandez <iah7731@rit.edu>
2024-09-30 18:58:20 -0400

    9/30/24 MVI Dash Update
    - Added the ability to dash (left shift).
    - Added a state list for the player.
    - Added a function that flips the player when moving in the opposite direction (will be useful for animation stuff later).
    - Implemented basic state changes (these will be expanded upon later).

---
+++ Darkrise/Assets/PlayerStateList.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateList : MonoBehaviour
+{
+    // this will be expanded a lot more after the MVI
+
+    public bool jumping = false;
+    public bool dashing = false;
+
+}
--- Darkrise/Assets/Scripts/PlayerController.cs
+++ Darkrise/Assets/Scripts/PlayerController.cs
@@ -9,27 +9,46 @@ public class PlayerController : MonoBehaviour
     [Header("Horizontal Movement Settings")]
     [SerializeField] private float walkSpeed;
     private float xAxis;
+    [Space(5)]
 
     [Header("Ground Check Settings")]
-
     [SerializeField] private float jumpForce;
     [SerializeField] private Transform groundCheckPoint;
     [SerializeField] private float groundCheckX;
     [SerializeField] private float groundCheckY;
     [SerializeField] private LayerMask groundLayer;
+    [Space(5)]
+
+    [Header("Dash Settings")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+    private bool canDash = true;
+    private bool dashed;
+    private float gravity;
+
+    PlayerStateList pState; // this will be expanded a lot more after the MVI
 
     // Start is called before the first frame update
     void Start()
     {
+        pState = GetComponent<PlayerStateList>();
+
         rb = GetComponent<Rigidbody2D>();
+
+        gravity = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInput();
+        UpdateJumpVariables();
+        if (pState.dashing) return; // if the player is dashing, don't get more movements
+        Flip();
         Move();
         Jump();
+        StartDash();
 
         // debugging code for collision detection stuff
         Debug.DrawRay(groundCheckPoint.position, Vector2.down * groundCheckY, Color.red);
@@ -41,10 +60,48 @@ public class PlayerController : MonoBehaviour
     {
         xAxis = Input.GetAxisRaw("Horizontal"); //default left/right keys are the arrow keys or A and D
     }
+    void Flip() // this will be useful for animation stuff later
+    {
+        if (xAxis < 0)
+        {
+            transform.localScale = new Vector2(-(Mathf.Abs(transform.localScale.x)), transform.localScale.y);
+        }
+        else if(xAxis > 0)
+        {
+            transform.localScale = new Vector2((Mathf.Abs(transform.localScale.x)), transform.localScale.y);
+        }
+    }
 
     void Move()
     {
         rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+        pState.dashing = true;
+        //trigger dash animation
+        rb.gravityScale = 0; // this allows the dash to keep the player from falling if they do so in the air
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = gravity;
+        pState.dashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    void StartDash()
+    {
+        if(Input.GetButtonDown("Dash") && canDash && !dashed) // dash key right now is left shift
+        {
+            StartCoroutine(Dash());
+            dashed = true;
+        }
+        if (Grounded())
+        {
+            dashed = false;
+        }
     }
 
     public bool Grounded()
@@ -58,8 +115,6 @@ public class PlayerController : MonoBehaviour
          * 
          * the other two checks add and subtract the groundCheckX variable to make sure that this works when the player is on the edge of a platform as well.
          */
-
-        
 
         if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, groundLayer)
             || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, groundLayer)
@@ -78,12 +133,23 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0) // jump button is spacebar
         {
             rb.velocity = new Vector3(rb.velocity.x, 0);
+
+            pState.jumping = false;
         }
 
 
         if (Input.GetButtonDown("Jump") && Grounded()) // jump button is spacebar
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+
+            pState.jumping = true;
+        }
+    }
+    void UpdateJumpVariables()
+    {
+        if(Grounded())
+        {
+            pState.jumping = false;
         }
     }
 }
