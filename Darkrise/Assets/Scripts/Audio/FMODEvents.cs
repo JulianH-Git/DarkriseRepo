0d9767250d82b153a86aee46e4d995d58c2898fe
GingeraleGrievous <gingeralegrievous@gmail.com>
2025-03-20 23:39:00 -0400

    FMOD is IN
    Added FMOD to the project
    
    all of the previous audio is now in FMOD mode!
    
    Sorry for the monster push

---
+++ Darkrise/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference music { get; private set; }
+
+    [field: Header("Player SFX")]
+
+    [field: SerializeField] public EventReference dash { get; private set; }
+
+    [field: SerializeField] public EventReference playerFootsteps { get; private set; }
+
+    [field: SerializeField] public EventReference normalSlash { get; private set; }
+
+    [field: SerializeField] public EventReference darkSlash { get; private set; }
+
+    [field: SerializeField] public EventReference lightSlash { get; private set; }
+
+    [field: SerializeField] public EventReference playerJump { get; private set; }
+
+    [field: SerializeField] public EventReference enemyHit { get; private set; }
+
+    public static FMODEvents instance { get;private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events instance in this scene.");
+        }
+        instance = this;
+    }
+}
