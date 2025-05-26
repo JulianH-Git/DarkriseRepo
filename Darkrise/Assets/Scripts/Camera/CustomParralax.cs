87d9c7b550ae1dab3e2e0da64eb9f14d66da58a2
Julian Howard <112177856+JulianH-Git@users.noreply.github.com>
2025-05-26 14:04:43 -0400

    Test Parallax Update
    Using a copied component that's present but unused on each tileset in the Unity Hierarchy, I was able to create a small example of a parallax effect on the building background. I tried making a custom version of said parallax to work with the virtual camera instead, but it didn't look correct to me so it's unused. The script is still there if anyone wants to try using it.

---
+++ Darkrise/Assets/Scripts/Camera/CustomParralax.cs
@@ -0,0 +1,130 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomParralax : MonoBehaviour
+{
+    // This is a custom version of LDtk to Unity's Parallax component script.
+    [Tooltip("Enable this on to mimic the parallax seen in LDtk.\n" +
+                     "It will try grabbing the MainCamera, but it can also be set in code.")]
+    public bool useParallax = true;
+
+    [Tooltip("Whether to use FixedUpdate; otherwise LateUpdate")]
+    public bool useFixedUpdate;
+    public Vector2 parallaxFactor;
+    public Vector2 halfLevelSize;
+    public bool scaled;
+    public GameObject cam;
+
+    private Transform _cameraTransform;
+    private Vector3 _prevPos;
+    private bool _prevUseParallax;
+    private Vector3 _delta;
+    //private Vector3 _speed;
+
+    private static Vector2 _minimumScale = new Vector2(0.01f, 0.01f);
+
+    internal void OnImport(Vector2 parallax, Vector2 halfLvlSize, bool isScaled)
+    {
+        parallaxFactor = parallax;
+        halfLevelSize = halfLvlSize;
+        scaled = isScaled;
+
+        if (scaled)
+        {
+            Vector2 inverseFactor = Vector2.one - parallaxFactor;
+            transform.position += (Vector3)(halfLevelSize * parallaxFactor);
+
+            inverseFactor = Vector2.Max(inverseFactor, _minimumScale);
+            Vector2 newScale = transform.localScale * new Vector2(inverseFactor.x, inverseFactor.y);
+            transform.localScale = new Vector3(newScale.x, newScale.y, transform.localScale.z);
+        }
+    }
+
+    private void Start()
+    {
+        //If already cached from external code, don't fight with it
+        if (_cameraTransform)
+        {
+            return;
+        }
+
+        if (cam)
+        {
+            SetCamera(cam.transform);
+            Center();
+        }
+    }
+
+    private void Center()
+    {
+        //setup the prev position as the center of the layer so its original position matches like LDtk
+        Vector2 scaledOffset = halfLevelSize;
+        if (scaled)
+        {
+            scaledOffset *= (Vector2.one - parallaxFactor);
+        }
+        _prevPos = (Vector2)transform.position + scaledOffset;
+
+        //set true to make the camera delta snap to where it should be like how it looks in ldtk
+        _prevUseParallax = true;
+    }
+
+    /// <summary>
+    /// Custom-controlled set.
+    /// </summary>
+    [PublicAPI]
+    public void SetCamera(Transform cam)
+    {
+        _cameraTransform = cam;
+    }
+
+    private void FixedUpdate()
+    {
+        if (useFixedUpdate)
+        {
+            UpdateParallax();
+        }
+    }
+    private void LateUpdate()
+    {
+        if (!useFixedUpdate)
+        {
+            UpdateParallax();
+        }
+    }
+
+    private void UpdateParallax()
+    {
+        if (!_cameraTransform)
+        {
+            return;
+        }
+
+        if (parallaxFactor == Vector2.zero)
+        {
+            return;
+        }
+
+        if (!Application.isPlaying && !useParallax)
+        {
+            return;
+        }
+
+        Vector3 cameraPos = _cameraTransform.position;
+
+        //only realize a difference if the parallax is enabled
+        if (useParallax && !_prevUseParallax)
+        {
+            _prevPos = cameraPos;
+        }
+        _prevUseParallax = useParallax;
+
+        //Delta of the camera from last update. If there was movement, move this object with the camera but of a certain multiplier. 
+        _delta = cameraPos - _prevPos;
+        transform.position += Vector3.Scale(_delta, parallaxFactor);
+
+        _prevPos = cameraPos;
+    }
+}
